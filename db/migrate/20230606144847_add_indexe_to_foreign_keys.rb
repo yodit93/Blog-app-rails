c7a5c15fbd2d26a537c804eb29f05fb19d286351
yodit93 <yoditabebe9391@gmail.com>
2023-06-06 18:11:04 +0300

    Add index for user table

--- db/migrate/20230606144847_add_indexe_to_foreign_keys.rb
+++ db/migrate/20230606144847_add_indexe_to_foreign_keys.rb
@@ -4,6 +4,5 @@ class AddIndexeToForeignKeys < ActiveRecord::Migration[7.0]
     add_index :comments, :post_id
     add_index :posts, :user_id
     add_index :likes, :user_id
-    add_index :likes, :post_id
   end
 end
