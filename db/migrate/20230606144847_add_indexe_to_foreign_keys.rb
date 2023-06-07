8701e0b95d40fa8a01ccacd623c62e85eaf08f92
yodit93 <yoditabebe9391@gmail.com>
2023-06-07 17:39:24 +0300

    Update Gemfiles and delete previous migration files

--- db/migrate/20230606144847_add_indexe_to_foreign_keys.rb
+++
@@ -1,8 +0,0 @@
-class AddIndexeToForeignKeys < ActiveRecord::Migration[7.0]
-  def change
-    add_index :comments, :user_id
-    add_index :comments, :post_id
-    add_index :posts, :user_id
-    add_index :likes, :user_id
-  end
-end
--- db/migrate/20230606150755_add_address_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddAddressToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :address, :string
-    add_index :users, :address
-  end
-end
