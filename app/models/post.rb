43b602940d1f0f8257145e70cf8e6d021a084a54
yodit93 <yoditabebe9391@gmail.com>
2023-06-08 16:13:55 +0300

    Update post & user with validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
-  validate :title, presence: true, length: { maximum: 250 }
-  validate :comments_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  validate :likes_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :likes_counter, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
   def user_posts_counter
     posts_counter = author.posts_counter || 0
