aecbf3a4eb14a6f9ca3677582737ece7e36d8e6e
yodit93 <yoditabebe9391@gmail.com>
2023-06-08 12:48:12 +0300

    Fix linter error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
-    belongs_to :post
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-    def post_comments_counter
-        comments_counter = post.comments_counter || 0
-        post.update(comments_counter: comments_counter + 1)
-    end
+  def post_comments_counter
+    comments_counter = post.comments_counter || 0
+    post.update(comments_counter: comments_counter + 1)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,14 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-    has_many :comments
-    has_many :likes
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments
+  has_many :likes
 
-    def user_posts_counter
-        posts_counter = author.posts_counter || 0
-        author.update(posts_counter: posts_counter + 1)
-    end
+  def user_posts_counter
+    posts_counter = author.posts_counter || 0
+    author.update(posts_counter: posts_counter + 1)
+  end
 
-    def recent_comments
-        comments.order(created_at: :desc).limit(5)
-    end
-
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
