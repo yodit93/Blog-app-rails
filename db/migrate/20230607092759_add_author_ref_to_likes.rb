d4243d0b274be05c5de942819743c9cc61a74d24
yodit93 <yoditabebe9391@gmail.com>
2023-06-08 10:36:15 +0300

    Create users

--- db/migrate/20230607092759_add_author_ref_to_likes.rb
+++ db/migrate/20230607092759_add_author_ref_to_likes.rb
@@ -1,4 +1,4 @@
-class AddUserRefToLikes < ActiveRecord::Migration[7.0]
+class AddAuthorRefToLikes < ActiveRecord::Migration[7.0]
   def change
     add_reference :likes, :author, null: false, foreign_key: { to_table: :users}
   end
