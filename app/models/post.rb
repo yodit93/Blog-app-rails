1ac2676625d94b1c0af6f64fa7a5500f53ac27e7
yodit93 <yoditabebe9391@gmail.com>
2023-06-08 12:03:49 +0300

    Add user_posts_counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+    def user_posts_counter
+        posts_counter = author.posts_counter || 0
+        author.update(posts_counter: posts_counter + 1)
+    end
 end
