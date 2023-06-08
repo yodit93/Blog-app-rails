1bbead6833d0a137872ebee412e6a4ad65fd705f
yodit93 <yoditabebe9391@gmail.com>
2023-06-08 16:12:53 +0300

    Add rspec test for like

---
+++ spec/model/like_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+describe Like do
+   describe "#post_likes_counter" do
+         it "should update the post likes_counter" do
+              user = User.create(name: "name", posts_counter: 0)
+              post = Post.create(title: "title", text: "text", author: user, comments_counter: 0, likes_counter: 0)
+              like = Like.create(author: user, post: post)
+              like.post_likes_counter
+              expect(post.likes_counter).to eq(1)
+         end
+    end
+end
