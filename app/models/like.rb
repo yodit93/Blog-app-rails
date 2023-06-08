6d45dca3cb48d7b8d4389db79334b96b88e51605
yodit93 <yoditabebe9391@gmail.com>
2023-06-08 12:36:12 +0300

    Add post likes counter for Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: :author_id
     belongs_to :post
+
+    def post_likes_counter
+        likes_counter = post.likes_counter || 0
+        post.update(likes_counter: likes_counter + 1)
+    end
 end
