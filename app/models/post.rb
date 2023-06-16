97b36b4e316bd474a2c2b2a9f5317d04c5967268
yodit93 <yoditabebe9391@gmail.com>
2023-06-16 11:59:27 +0300

    Update user posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,8 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
+  after_save :user_posts_counter
+  
   def user_posts_counter
     posts_counter = author.posts_counter || 0
     author.update(posts_counter: posts_counter + 1)
