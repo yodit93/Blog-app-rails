591c26220115af012f916688b2407c75fec5f782
yodit93 <yoditabebe9391@gmail.com>
2023-06-16 11:56:20 +0300

    Add new and create method for comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+    def new
+        @user = current_user
+        @post = @user.posts.find(params[:post_id])
+        @comment =  Comment.new
+    end
+
+    def create
+        @user = current_user
+        @post = @user.posts.find(params[:post_id])
+        @comment = Comment.new(author: @user, post: @post, text: params[:comment][:text])
+        if @comment.save
+            redirect_to user_post_path(@user, @post), notice: 'Comment created successfully.'
+        else
+            render :new
+        end
+    end
 end
