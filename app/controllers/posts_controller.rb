b7d2afc7308f4365ce2ff56e1d938467cc0cc8fe
yodit93 <yoditabebe9391@gmail.com>
2023-06-16 12:01:29 +0300

    Add likes form as partial inside posts/show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,8 +24,9 @@ class PostsController < ApplicationController
     end
   end
 
-  private def post_params
+  private
+
+  def post_params
     params.require(:post).permit(:title, :text)
   end
 end
-
