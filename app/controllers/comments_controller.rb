223ddad7e95cdb944071feaed855aab3f004d7d1
yodit93 <yoditabebe9391@gmail.com>
2023-06-22 22:11:03 +0300

    Update create comment action to diferentiate the current user from the author

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,14 @@
 class CommentsController < ApplicationController
   def new
-    @user = current_user
-    @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
 
   def create
-    @user = current_user
+    @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:post_id])
-    @comment = Comment.new(author: @user, post: @post, text: params[:comment][:text])
+    @comment = Comment.new(author: current_user, post: @post, text: params[:comment][:text])
     if @comment.save
-      redirect_to user_post_path(@user, @post), notice: 'Comment created successfully.'
+      redirect_to user_post_path(params[:user_id], @post), notice: 'Comment created successfully.'
     else
       render :new
     end
