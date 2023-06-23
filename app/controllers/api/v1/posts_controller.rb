35794c598533f6af8121c3695f99bd865b8e0561
yodit93 <yoditabebe9391@gmail.com>
2023-06-23 14:21:59 +0300

    Create an API endpoint to list all posts for a user

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::PostsController < ApplicationController
+    def index
+        @posts = Post.all
+        render json: @posts
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        render json: @posts
+    end
 end
