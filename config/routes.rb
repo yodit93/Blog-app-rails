a42fd01158eb1b329a07c5e9a64e204faa4c5785
yodit93 <yoditabebe9391@gmail.com>
2023-06-23 12:44:11 +0300

    Wrap the resources routes inside api/v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "users#index"
-  resources :users, only: [:show] do
-    resources :posts do
-      resources :comments, only: [:new, :create, :destroy]
-      resources :likes, only: [:create]
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:show] do
+        resources :posts do
+          resources :comments, only: [:new, :create, :destroy]
+          resources :likes, only: [:create]
+        end
+      end
     end
-  end 
+  end
 end
