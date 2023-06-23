03acb21987536a82903c823a06856f766de5e608
yodit93 <yoditabebe9391@gmail.com>
2023-06-23 15:06:34 +0300

    Update api/v1/posts controller index for listing specific user posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::PostsController < ApplicationController
     def index
-        @posts = Post.all
+        @user = User.find(params[:user_id])
+        @posts = @user.posts
         render json: @posts
     end
 
