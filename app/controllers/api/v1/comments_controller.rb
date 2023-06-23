9afe8f6aa2300701de362693d2f15ad47853ddb6
theophilus doku <theodoku89@gmail.com>
2023-06-23 14:41:13 +0000

    update comments_controller.rb file

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,8 +1,24 @@
 class Api::V1::CommentsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:post_id])
-    @comments = @post.comments
-    render json: @comments
+    user = User.find(params[:user_id])
+    post = user.posts.find(params[:post_id])
+    comments = post.comments
+    render json: comments
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = post.comments.build(comment_params)
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
