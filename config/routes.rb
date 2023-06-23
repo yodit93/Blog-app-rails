810de513c14869d4f8cb98588bdc6348306f835c
yodit93 <yoditabebe9391@gmail.com>
2023-06-23 13:56:21 +0300

    Include users#index to users routes to get list of users from api

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "users#index"
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:show] do
+      resources :users, only: [:index, :show] do
         resources :posts do
           resources :comments, only: [:new, :create, :destroy]
           resources :likes, only: [:create]
